556c24225cf48ad6e1f25ced48aa9ce6fac35d45
akifpatel8 <aakifpatel08@gmail.com>
2025-08-19 22:53:38 +0530

    fixed bug

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -4,14 +4,20 @@ import { useEffect } from "react"
 const RestaurantMenu = () => {
     const { id } = useParams()
 
-    useEffect((
+    useEffect(() => {
         getRestaurauntInfo()
-    ), [])
+    },[])
 
     async function getRestaurauntInfo() {
-        let res = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=10576&catalog_qa=undefined&submitAction=ENTER")
-        let data = await res.json()
-        console.log(data?.data)
+        try {   
+            let res = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=10576&catalog_qa=undefined&submitAction=ENTER")
+            let data = await res.json()
+            console.log("hello",data?.data?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[1]?.card?.card?.itemCards);
+            
+            console.log(data?.data)
+        } catch(error) {
+            console.log(error,"error");
+        }
     }
     return (
         <>
